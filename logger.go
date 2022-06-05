41b890b3faab971b6d9d0f14cd8e8fbf31b8b61c
Cenk Kılıç <cenk@kilic.dev>
2022-06-05 23:47:37 +0200

    fix: add default level

--- logger.go
+++ logger.go
@@ -7,11 +7,12 @@ type (
 )
 
 const (
-	LOG_LEVEL_PANIC LogLevel = logrus.PanicLevel
-	LOG_LEVEL_FATAL LogLevel = logrus.FatalLevel
-	LOG_LEVEL_ERROR LogLevel = logrus.ErrorLevel
-	LOG_LEVEL_WARN  LogLevel = logrus.WarnLevel
-	LOG_LEVEL_INFO  LogLevel = logrus.InfoLevel
-	LOG_LEVEL_DEBUG LogLevel = logrus.DebugLevel
-	LOG_LEVEL_TRACE LogLevel = logrus.TraceLevel
+	LOG_LEVEL_DEFAULT LogLevel = 0
+	LOG_LEVEL_PANIC   LogLevel = logrus.PanicLevel
+	LOG_LEVEL_FATAL   LogLevel = logrus.FatalLevel
+	LOG_LEVEL_ERROR   LogLevel = logrus.ErrorLevel
+	LOG_LEVEL_WARN    LogLevel = logrus.WarnLevel
+	LOG_LEVEL_INFO    LogLevel = logrus.InfoLevel
+	LOG_LEVEL_DEBUG   LogLevel = logrus.DebugLevel
+	LOG_LEVEL_TRACE   LogLevel = logrus.TraceLevel
 )
