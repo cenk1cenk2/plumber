3f8b0b27f7ad49ce7c8caff2598300cc5d36dbe0
Cenk Kılıç <cenk@kilic.dev>
2022-09-29 17:52:47 +0200

    fix: update documentation generation

--- flags.go
+++ flags.go
@@ -5,24 +5,30 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const CLI_FLAGS_CATEGORY = "CLI"
+
 var CliDefaultFlags = []cli.Flag{
 	&cli.BoolFlag{
-		Name:    "ci",
-		Usage:   "Sets whether this is running inside a CI/CD environment.",
-		Hidden:  true,
-		EnvVars: []string{"CI"},
+		Category: CLI_FLAGS_CATEGORY,
+		Name:     "ci",
+		Usage:    "Sets whether this is running inside a CI/CD environment.",
+		Hidden:   true,
+		EnvVars:  []string{"CI"},
 	},
 
 	&cli.BoolFlag{
-		Name:    "debug",
-		Usage:   "Enable debugging for the application.",
-		EnvVars: []string{"DEBUG"},
+		Category: CLI_FLAGS_CATEGORY,
+		Name:     "debug",
+		Usage:    "Enable debugging for the application.",
+		EnvVars:  []string{"DEBUG"},
+		Hidden:   true,
 	},
 
 	&cli.StringFlag{
-		Name:    "log-level",
-		Usage:   `Define the log level for the application. enum("PANIC", "FATAL", "WARNING", "INFO", "DEBUG", "TRACE")`,
-		EnvVars: []string{"LOG_LEVEL"},
-		Value:   logrus.InfoLevel.String(),
+		Category: CLI_FLAGS_CATEGORY,
+		Name:     "log-level",
+		Usage:    `Define the log level for the application. enum("PANIC", "FATAL", "WARNING", "INFO", "DEBUG", "TRACE")`,
+		EnvVars:  []string{"LOG_LEVEL"},
+		Value:    logrus.InfoLevel.String(),
 	},
 }
